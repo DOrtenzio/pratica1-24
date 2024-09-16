3acddfdd63897684f1c91dc0854d812a7046b42a
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-16 12:55:41 +0200

    terzo metodo

--- src/concessionario/funzioniClass.java
+++ src/concessionario/funzioniClass.java
@@ -2,7 +2,7 @@ package concessionario;
 
 public class funzioniClass {
     //aggiunta di una nuova auto
-    public int aggAuto (String [] marca,String [] modello,int [] prezzo, int index, String marcaNuova, String modelloNuovo, int prezzoNuovo){
+    public int aggAuto (String [] marca,String [] modello,double [] prezzo, int index, String marcaNuova, String modelloNuovo, double prezzoNuovo){
         index+=1;
         marca[index]=marcaNuova;
         modello[index]=modelloNuovo;
@@ -10,10 +10,19 @@ public class funzioniClass {
         return index;
     }
     //Visualizzazione di tutte le auto
-    public String visualizzaAuto (String [] marca,String [] modello,int [] prezzo, int index){
+    public String visualizzaAuto (String [] marca,String [] modello,double [] prezzo, int index){
         String s="";
         for (int i=0;i<index;i++){
             s=s+marca[i]+"\t"+modello[i]+"\t"+prezzo[i]+"\t\n";
+        }
+        return s;
+    }
+    // ricerca di un'auto tramite marca e modello
+    public String ricercaMarcaModello(String [] marca,String [] modello,double [] prezzo, int index,String marcaNuova, String modelloNuovo){
+        String s="";
+        for (int i=0;i<index;i++){
+            if (marca[i]==marcaNuova && modello[i]==modelloNuovo)
+                s=s+marca[i]+"\t"+modello[i]+"\t"+prezzo[i]+"\t\n";
         }
         return s;
     }
