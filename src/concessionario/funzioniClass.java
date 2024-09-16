370c27f1b4fb22348dce9ebf099dc996c8f860f8
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-09-16 19:17:13 +0200

    Update funzioniClass.java

--- src/concessionario/funzioniClass.java
+++ src/concessionario/funzioniClass.java
@@ -59,18 +59,19 @@ public class funzioniClass {
         return -1;
     }
     //Modifica
-    public static String modificaDati (String [] marca,String [] modello,double [] prezzo, int index, String marcaNuova, String modelloNuovo, double prezzoNuovo){
+    public static String modificaDati (String [] marca,String [] modello,double [] prezzo, int index, String marcaNuova, String modelloNuovo, double prezzoNuovo,String marcaNuova1, String modelloNuovo1, double prezzoNuovo1){
         int indice=ricercaIndiceAuto(marca,modello, prezzo,  index, marcaNuova,  modelloNuovo,  prezzoNuovo);
         String s="";
         if (indice==-1) //Se l'auto non esiste
             return "Non trovata";
         else { //Se l'auto esiste
-            marca[indice]=marcaNuova;
-            modello[indice]=modelloNuovo;
-            prezzo[indice]=prezzoNuovo;
+            marca[indice]=marcaNuova1;
+            modello[indice]=modelloNuovo1;
+            prezzo[indice]=prezzoNuovo1;
         }
         return "Modificato";
     }
+    //Non funziona
     //Visualizzazione di tutte le auto in ordine crescente non modifica in ordine crescente
     public static String visualizzaAutoCrescente (String [] marca,String [] modello,double [] prezzo, int index){
         //Variabili d'appoggio
