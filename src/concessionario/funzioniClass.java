943c3055ab42fb70d3bcd334ba0ea0e0c374971b
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-16 13:17:27 +0200

    quarto metodo

--- src/concessionario/funzioniClass.java
+++ src/concessionario/funzioniClass.java
@@ -21,9 +21,41 @@ public class funzioniClass {
     public String ricercaMarcaModello(String [] marca,String [] modello,double [] prezzo, int index,String marcaNuova, String modelloNuovo){
         String s="";
         for (int i=0;i<index;i++){
-            if (marca[i]==marcaNuova && modello[i]==modelloNuovo)
-                s=s+marca[i]+"\t"+modello[i]+"\t"+prezzo[i]+"\t\n";
+            if (marca[i].equalsIgnoreCase(marcaNuova) && modello[i].equalsIgnoreCase(modelloNuovo)) {
+                s = s + marca[i] + "\t" + modello[i] + "\t" + prezzo[i] + "\t\n";
+            }
         }
+        if (s.equalsIgnoreCase(""))
+            s="Non trovata";
         return s;
+    }
+    //cancellazione di un'auto
+    public int cancellaAuto (String [] marca,String [] modello,double [] prezzo, int index, String marcaNuova, String modelloNuovo, double prezzoNuovo){
+        int indice=ricercaIndiceAuto(marca,modello, prezzo,  index, marcaNuova,  modelloNuovo,  prezzoNuovo);
+        if (indice==-1) //Se l'auto non esiste
+            return index;
+        else { //Se l'auto esiste
+            for (int i=indice;i<=index;i++){ //Rimuovo
+                if (i==index){ //Se siamo all'ultimo
+                    marca[i]="";
+                    modello[i]="";
+                    prezzo[i]=0.0;
+                }else { //Prendo l'elemento prima
+                    marca[i]=marca[i+1];
+                    modello[i]=modello[i+1];
+                    prezzo[i]=prezzo[i+1];
+                }
+            }
+            return index-1;
+        }
+    }
+    // ricerca di un'auto tramite info
+    private int ricercaIndiceAuto(String [] marca,String [] modello,double [] prezzo, int index,String marcaNuova, String modelloNuovo, double prezzoNuovo){
+        for (int i=0;i<index;i++){
+            if (marca[i].equalsIgnoreCase(marcaNuova) && modello[i].equalsIgnoreCase(modelloNuovo) && prezzo[i]==prezzoNuovo) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
